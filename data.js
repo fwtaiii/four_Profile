acef441f731403a3c2b42ccdd12fff6189888878
four <614259038@webmail.npru.ac.th>
2020-11-11 12:46:38 +0700

    four

---
+++ data.js
@@ -0,0 +1,20 @@
+const data = {
+    "name": "naruecha thanompaladisai",
+    "aboutme":{
+        "FullName":"naruecha",
+        "Phone":"0983203564",
+        "Email":"614259038@webmail.npru.ac.th",
+        "Address":"122, huayphra , DonTum , Nakhonpathom 73150",
+    
+    },
+    "education":{
+        "Upper":"Khongtong English-chinese class",
+        "lowers":"Khongtong"
+    },
+    "workexperience":{
+      "1":"play Sports.",
+      "2":"musician in my spare time."
+    }
+
+};
+module.exports = data;
